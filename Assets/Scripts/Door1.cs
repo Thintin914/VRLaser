873f0a4d0e8443859a35cbabee96024bac51aff1
Thintin914 <ttca914@gmail.com>
2021-11-24 17:02:22 +0800

    Initial commit

---
+++ Assets/Scripts/Door1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door1 : MonoBehaviour
+{
+    public Transform door1Model, door2Model;
+    public int openRate;
+
+    private void Start()
+    {
+        if (GetComponent<SubscribeExtraInformation>().extraSettings.Count > 0)
+        {
+            List<string> extraSettings = GetComponent<SubscribeExtraInformation>().extraSettings;
+            GetComponent<SceneObjectTag>().sceneTag = extraSettings[0];
+            openRate = int.Parse(extraSettings[1]);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        StartCoroutine(MoveDoor(true));
+    }
+    public void CloseDoor()
+    {
+        StartCoroutine(MoveDoor(false));
+    }
+
+    IEnumerator MoveDoor(bool isOpen)
+    {
+        if (isOpen)
+        {
+            for (int i = 0; i < openRate; i++)
+            {
+                yield return new WaitForSeconds(Time.deltaTime);
+                door1Model.position += transform.right * 0.05f;
+                door2Model.position += -transform.right * 0.05f;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < openRate; i++)
+            {
+                yield return new WaitForSeconds(Time.deltaTime);
+                door1Model.position += -transform.right * 0.05f;
+                door2Model.position += transform.right * 0.05f;
+            }
+        }
+        LaserInteraction.hasSceneUpdate = true;
+    }
+}
---
+++ Assets/Scripts/VRLeftHand.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRLeftHand : MonoBehaviour
+{
+    public VRRightHand rightHand;
+    public SteamVR_Action_Boolean zoom, menu;
+    public GameObject playerMenu;
+    private GameObject cam;
+    private Vector3 finalPosition, finalDirection;
+
+    private void Awake()
+    {
+        cam = Camera.main.gameObject;
+        for(int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.layer = LayerMask.NameToLayer("PlayerMenu");
+        }
+        gameObject.layer = LayerMask.NameToLayer("PlayerMenu");
+    }
+
+    private void Update()
+    {
+            if (rightHand.holdingObject != null && rightHand.holdingObjectInteraction == VRRightHand.HoldingObjectType.pickup)
+            {
+                if (zoom.stateDown) {
+                    rightHand.PushHoldingObject();
+                }
+            }
+            if (menu.stateDown)
+            {
+                finalPosition = cam.transform.position + cam.transform.forward * 0.55f + cam.transform.up * -0.2f;
+                finalDirection = (finalPosition - cam.transform.position).normalized;
+                playerMenu.transform.position = finalPosition;
+                playerMenu.transform.rotation = Quaternion.LookRotation(finalDirection, Vector3.up);
+            }
+    }
+}
